cf2763da3a77efe4db6fa6a293617b366ad81da6
Jonghyuck Park @Home <nrevival@gmail.com>
2019-09-17 22:34:27 +0900

    devel 작성중, 다음에는 나는 누구인가 페이지 완성필요

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,8 +13,7 @@ module.exports = {
     menu: siteConfig.menu,
     author: siteConfig.author
   },
-  plugins: [
-    'gatsby-plugin-robots-txt',
+  plugins: [    
     {
       resolve: 'gatsby-source-filesystem',
       options: {
@@ -188,5 +187,22 @@ module.exports = {
       }
     },
     'gatsby-plugin-flow',
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+        {
+          resolve: "gatsby-remark-embed-video",
+          options: {
+            width: 800,
+            ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
+            height: 400, // Optional: Overrides optional.ratio
+            related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
+            noIframeBorder: true //Optional: Disable insertion of <style> border: 0
+          }
+        }
+        ]
+      }
+    },
   ]
 };
